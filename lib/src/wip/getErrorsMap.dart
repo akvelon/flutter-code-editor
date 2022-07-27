10255001b841cf08231fc19c808c6103fc510327
Darkhan Nausharipov <nausharipov@gmail.com>
2022-07-27 11:49:33 +0600

    disabled countingBrackets

--- lib/src/wip/getErrorsMap.dart
+++ lib/src/wip/getErrorsMap.dart
@@ -5,7 +5,6 @@ import 'package:highlight/languages/java.dart';
 import 'package:highlight/languages/python.dart';
 import 'package:highlight/languages/scala.dart';
 
-import 'language_syntax/brackets_counting.dart';
 import 'language_syntax/golang_syntax.dart';
 import 'language_syntax/java_dart_syntax.dart';
 import 'language_syntax/python_syntax.dart';
@@ -13,7 +12,10 @@ import 'language_syntax/scala_syntax.dart';
 
 Map<int, String> getErrorsMap(String text, Mode? language) {
   Map<int, String> errors = {};
-  errors.addAll(countingBrackets(text));
+
+  /// This check is temporarily disabled because unclosed ' and "
+  /// characters in comments cause false positive errors
+  // errors.addAll(countingBrackets(text));
 
   if (language == java || language == dart) {
     errors.addAll(findJavaDartErrors(text));
