f3245630ba593e7fb54915acc0972901b31b1e1f
Alexey Inkin <alexey.inkin@akvelon.com>
2022-08-08 16:17:59 +0400

    Dampen changes (#39)

--- test/src/code/code_test.dart
+++ test/src/code/code_test.dart
@@ -12,8 +12,8 @@ final _language = java;
 const _text = '''
 1 Lorem ipsum dolor sit amet,
 2 consectetur adipiscing elit,
-3 sed do eiusmod tempor incididunt ut labore et dolore magna.
-4 Ut enim ad minim veniam,
+3 sed do eiusmod tempor incididunt ut labore et dolore magna
+4 aliqua. Ut enim ad minim veniam,
 5 quis nostrud exercitation ullamco laboris nisi
 6 ut aliquip
 7 ex
@@ -68,86 +68,86 @@ void main() {
             30,
           ),
 
-          // 62
+          // 61
           CodeLine.fromTextAndStart(
-            '3 sed do eiusmod tempor incididunt ut labore et dolore magna.\n',
+            '3 sed do eiusmod tempor incididunt ut labore et dolore magna\n',
             61,
           ),
 
-          // 27
+          // 35
           CodeLine.fromTextAndStart(
-            '4 Ut enim ad minim veniam,\n',
-            123,
+            '4 aliqua. Ut enim ad minim veniam,\n',
+            122,
           ),
 
           // 49
           CodeLine.fromTextAndStart(
             '5 quis nostrud exercitation ullamco laboris nisi\n',
-            150,
+            157,
           ),
 
           // 13
           CodeLine.fromTextAndStart(
             '6 ut aliquip\n',
-            199,
+            206,
           ),
 
           // 5
           CodeLine.fromTextAndStart(
             '7 ex\n',
-            212,
+            219,
           ),
 
           // 1
           CodeLine.fromTextAndStart(
             '\n',
-            217,
+            224,
           ),
 
           // 24
           CodeLine.fromTextAndStart(
             '9 ea commodo consequat.\n',
-            218,
+            225,
           ),
 
           // 28
           CodeLine.fromTextAndStart(
             '10 Duis aute irure dolor in\n',
-            242,
+            249,
           ),
 
           // 55
           CodeLine.fromTextAndStart(
             '11 reprehenderit in voluptate velit esse cillum dolore\n',
-            270,
+            277,
           ),
 
           // 29
           CodeLine.fromTextAndStart(
             '12 eu fugiat nulla pariatur.\n',
-            325,
+            332,
           ),
 
           // 37
           CodeLine.fromTextAndStart(
             '13 Excepteur sint occaecat cupidatat\n',
-            354,
+            361,
           ),
 
           // 17
           CodeLine.fromTextAndStart(
             '14 non proident,\n',
-            391,
+            398,
           ),
 
           // 65
           CodeLine.fromTextAndStart(
             '15 sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            408,
+            415,
           ),
         ],
       );
-      expect(code.lines.last.textRange.end, 473);
+      expect(code.lines.last.textRange.end, 480);
     });
 
     test('characterIndexToLineIndex', () {
@@ -165,31 +165,31 @@ void main() {
           30: 1,
           60: 1,
           61: 2,
-          122: 2,
-          123: 3,
-          149: 3,
-          150: 4,
-          198: 4,
-          199: 5,
-          211: 5,
-          212: 6,
-          216: 6,
-          217: 7,
-          218: 8,
-          241: 8,
-          242: 9,
-          269: 9,
-          270: 10,
-          324: 10,
-          325: 11,
-          353: 11,
-          354: 12,
-          390: 12,
-          391: 13,
-          407: 13,
-          408: 14,
-          473: 14, // Equal to length without tail
-          if (tail == '\n') 474: 15, // Equal to length with tail
+          121: 2,
+          132: 3,
+          156: 3,
+          157: 4,
+          205: 4,
+          206: 5,
+          218: 5,
+          219: 6,
+          223: 6,
+          224: 7,
+          225: 8,
+          248: 8,
+          249: 9,
+          276: 9,
+          277: 10,
+          331: 10,
+          332: 11,
+          360: 11,
+          361: 12,
+          397: 12,
+          398: 13,
+          414: 13,
+          415: 14,
+          480: 14, // Equal to length without tail
+          if (tail == '\n') 481: 15, // Equal to length with tail
         };
 
         for (final entry in map.entries) {
