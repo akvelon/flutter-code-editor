293468b4aad1f68945b6fd2e6fc49ba5ad68167d
Alexey Inkin <alexey.inkin@akvelon.com>
2022-08-26 15:00:26 +0400

    Reuse const loremIpsom (#23)

--- test/src/autocomplete/autocompleter_test.dart
+++ test/src/autocomplete/autocompleter_test.dart
@@ -3,22 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:highlight/languages/dart.dart';
 import 'package:highlight/languages/java.dart';
 
-const loremIpsum = '''
-1 Lorem ipsum dolor sit amet,
-2 consectetur adipiscing elit,
-3 sed do eiusmod tempor incididunt ut labore et dolore magna
-4 aliqua. Ut enim ad minim veniam,
-5 quis nostrud exercitation ullamco laboris nisi
-6 ut aliquip
-7 ex
-
-9 ea commodo consequat.
-10 Duis aute irure dolor in
-11 reprehenderit in voluptate velit esse cillum dolore
-12 eu fugiat nulla pariatur.
-13 Excepteur sint occaecat cupidatat
-14 non proident,
-15 sunt in culpa qui officia deserunt mollit anim id est laborum.''';
+import '../common/lorem_ipsum.dart';
 
 const _edgeText = r'''
 'Single_quotes1 single_quotes2' 'single_quotes3'
