f1ee1eee0dadc3f0eae6e45a0e32ebafdfd0bb34
malarg <mr.malarg@gmail.com>
2022-10-10 14:35:33 +0400

    issue_47 another fix tests

--- test/src/folding/foldable_block_test.dart
+++ test/src/folding/foldable_block_test.dart
@@ -23,33 +23,8 @@ void main() {
       expect(actual, blocks);
     });
 
-    test('Join intersecting blocks in list with two intersecting blocks', () {
-      const blocks = [
-        FoldableBlock(startLine: 0, endLine: 1, type: FBT.braces),
-        FoldableBlock(startLine: 1, endLine: 2, type: FBT.braces),
-      ];
-      const expected = [
-        FoldableBlock(startLine: 0, endLine: 2, type: FBT.union),
-      ];
 
-      final actual = [...blocks]..joinIntersecting();
-
-      expect(actual, expected);
-    });
-
-    test('Join intersecting blocks in list with two not intersecting blocks',
-        () {
-      const blocks = [
-        FoldableBlock(startLine: 0, endLine: 1, type: FBT.braces),
-        FoldableBlock(startLine: 2, endLine: 3, type: FBT.braces),
-      ];
-
-      final actual = [...blocks]..joinIntersecting();
-
-      expect(actual, blocks);
-    });
-
-    test('Multiple not intersected blocks will not join', () {
+    test('Not intersected blocks do not join', () {
       const blocks = [
         FB(startLine: 0, endLine: 3, type: FBT.braces),
         FB(startLine: 4, endLine: 5, type: FBT.imports),
@@ -61,22 +36,8 @@ void main() {
       expect(actual, blocks);
     });
 
-    test('Multiple intersected blocks will join', () {
-      const blocks = [
-        FB(startLine: 0, endLine: 3, type: FBT.braces),
-        FB(startLine: 2, endLine: 5, type: FBT.imports),
-        FB(startLine: 4, endLine: 10, type: FBT.parentheses),
-      ];
-      const expected = [
-        FB(startLine: 0, endLine: 10, type: FBT.union),
-      ];
 
-      final actual = [...blocks]..joinIntersecting();
-
-      expect(actual, expected);
-    });
-
-    test('Mixed blocks will join correctly', () {
+    test('Mixed blocks join correctly', () {
       const blocks = [
         FB(startLine: 0, endLine: 3, type: FBT.braces),
         FB(startLine: 2, endLine: 5, type: FBT.imports),
@@ -85,6 +46,21 @@ void main() {
       const expected = [
         FB(startLine: 0, endLine: 5, type: FBT.union),
         FB(startLine: 6, endLine: 10, type: FBT.parentheses),
+      ];
+
+      final actual = [...blocks]..joinIntersecting();
+
+      expect(actual, expected);
+    });
+
+    test('Multiple intersected blocks will join in single', () {
+      const blocks = [
+        FB(startLine: 0, endLine: 3, type: FBT.braces),
+        FB(startLine: 2, endLine: 5, type: FBT.imports),
+        FB(startLine: 4, endLine: 10, type: FBT.parentheses),
+      ];
+      const expected = [
+        FB(startLine: 0, endLine: 10, type: FBT.union),
       ];
 
       final actual = [...blocks]..joinIntersecting();
@@ -124,15 +100,15 @@ void main() {
     // 10       |   |
     // 11       |   |
     // 12       |
-    // 
+    //
     // blocks[0] and blocks[3] should be joined, but they aren't.
     test('Containing children blocks will not join', () {
       const blocks = [
-        FB(startLine: 0, endLine: 6, type: FBT.braces),        // 0
-        FB(startLine: 1, endLine: 3, type: FBT.parentheses),   // 1
-        FB(startLine: 4, endLine: 5, type: FBT.parentheses),   // 2
-        FB(startLine: 6, endLine: 12, type: FBT.braces),       // 3
-        FB(startLine: 7, endLine: 9, type: FBT.parentheses),   // 4 
+        FB(startLine: 0, endLine: 6, type: FBT.braces), // 0
+        FB(startLine: 1, endLine: 3, type: FBT.parentheses), // 1
+        FB(startLine: 4, endLine: 5, type: FBT.parentheses), // 2
+        FB(startLine: 6, endLine: 12, type: FBT.braces), // 3
+        FB(startLine: 7, endLine: 9, type: FBT.parentheses), // 4
         FB(startLine: 10, endLine: 11, type: FBT.parentheses), // 5
       ];
 
