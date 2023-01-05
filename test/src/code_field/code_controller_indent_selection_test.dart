269b05863ffd99293adf8329a5aecf5df96111f1
Yestay Tastanov <yestay@Yestays-MacBook-Pro.local>
2023-01-05 21:46:47 +0600

    test fixes

--- test/src/code_field/code_controller_indent_selection_test.dart
+++ test/src/code_field/code_controller_indent_selection_test.dart
@@ -296,87 +296,31 @@ ${indent}AAA
           'Indent SHOULD NOT unfold folded comment at line 0 '
           'and folded imports',
           initialFullText: '''
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// comment1
+// comment 2
 package org.apache.beam.examples;
 
 import java.util.Arrays;
-import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.io.TextIO;
-import org.apache.beam.sdk.options.PipelineOptions;
-import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.Count;
-import org.apache.beam.sdk.transforms.Filter;
-import org.apache.beam.sdk.transforms.FlatMapElements;
-import org.apache.beam.sdk.transforms.MapElements;
-import org.apache.beam.sdk.values.KV;
-import org.apache.beam.sdk.values.TypeDescriptors;
 
-aaaa {
-
-}
+a;
 ''',
           expectedFullText: '''
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// comment1
+// comment 2
 package org.apache.beam.examples;
 
 import java.util.Arrays;
-import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.io.TextIO;
-import org.apache.beam.sdk.options.PipelineOptions;
-import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.Count;
-import org.apache.beam.sdk.transforms.Filter;
-import org.apache.beam.sdk.transforms.FlatMapElements;
-import org.apache.beam.sdk.transforms.MapElements;
-import org.apache.beam.sdk.values.KV;
-import org.apache.beam.sdk.values.TypeDescriptors;
 
-  aaaa {
-
-  }
+  a;
 ''',
           expectedVisibleText: '''
-/*
+// comment1
 package org.apache.beam.examples;
 
-  aaaa {
-
-  }
+  a;
 ''',
-          initialSelection: TextSelection(baseOffset: 38, extentOffset: 47),
-          expectedSelection: TextSelection(baseOffset: 38, extentOffset: 52),
+          initialSelection: TextSelection(baseOffset: 47, extentOffset: 49),
+          expectedSelection: TextSelection(baseOffset: 47, extentOffset: 52),
         ),
       ];
 
@@ -389,7 +333,6 @@ package org.apache.beam.examples;
         controller.foldImports();
         controller.selection = example.initialSelection;
 
-        // act
         controller.indentSelection();
 
         expect(
@@ -415,6 +358,7 @@ package org.apache.beam.examples;
       }
     });
   });
+
   group('Readonly blocks', () {
     final language = java;
     const readonlySectionName = 'readonlySection'; // length = 15
--- test/src/code_field/code_controller_outdent_selection_test.dart
+++ test/src/code_field/code_controller_outdent_selection_test.dart
@@ -262,87 +262,31 @@ AAAA
           'Outdent SHOULD NOT unfold folded comment at line 0 '
           'and folded imports',
           initialFullText: '''
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// comment1
+// comment 2
 package org.apache.beam.examples;
 
 import java.util.Arrays;
-import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.io.TextIO;
-import org.apache.beam.sdk.options.PipelineOptions;
-import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.Count;
-import org.apache.beam.sdk.transforms.Filter;
-import org.apache.beam.sdk.transforms.FlatMapElements;
-import org.apache.beam.sdk.transforms.MapElements;
-import org.apache.beam.sdk.values.KV;
-import org.apache.beam.sdk.values.TypeDescriptors;
 
-  aaaa {
-
-  }
+  a;
 ''',
           expectedFullText: '''
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// comment1
+// comment 2
 package org.apache.beam.examples;
 
 import java.util.Arrays;
-import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.io.TextIO;
-import org.apache.beam.sdk.options.PipelineOptions;
-import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.Count;
-import org.apache.beam.sdk.transforms.Filter;
-import org.apache.beam.sdk.transforms.FlatMapElements;
-import org.apache.beam.sdk.transforms.MapElements;
-import org.apache.beam.sdk.values.KV;
-import org.apache.beam.sdk.values.TypeDescriptors;
 
-aaaa {
-
-}
+a;
 ''',
           expectedVisibleText: '''
-/*
+// comment1
 package org.apache.beam.examples;
 
-aaaa {
-
-}
+a;
 ''',
-          initialSelection: TextSelection(baseOffset: 38, extentOffset: 51),
-          expectedSelection: TextSelection(baseOffset: 38, extentOffset: 48),
+          initialSelection: TextSelection(baseOffset: 47, extentOffset: 52),
+          expectedSelection: TextSelection(baseOffset: 47, extentOffset: 50),
         ),
       ];
 
