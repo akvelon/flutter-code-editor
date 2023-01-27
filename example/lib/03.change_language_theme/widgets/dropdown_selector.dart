847d4d2c5731a32f4ed4f0e404a430ad43ca1d0c
Yestay Tastanov <yestay@Yestays-MacBook-Pro.local>
2023-01-27 11:57:14 +0600

    example 03 restored

---
+++ example/lib/03.change_language_theme/widgets/dropdown_selector.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class DropdownSelector extends StatelessWidget {
+  final IconData icon;
+  final ValueChanged<String> onChanged;
+  final String value;
+  final Iterable<String> values;
+
+  const DropdownSelector({
+    required this.icon,
+    required this.onChanged,
+    required this.value,
+    required this.values,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: value,
+      items: values.map((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value, style: const TextStyle(color: Colors.white)),
+        );
+      }).toList(growable: false),
+      icon: Icon(icon, color: Colors.white),
+      onChanged: (value) {
+        if (value != null) onChanged(value);
+      },
+      dropdownColor: Colors.black87,
+    );
+  }
+}
