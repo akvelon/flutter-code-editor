a9e5fe9e1179d9fdba537419c37a7b0a5d55e648
Yestay Tastanov <yestay@Yestays-MacBook-Pro.local>
2023-02-02 11:45:52 +0600

    lint

--- lib/src/gutter/error.dart
+++ lib/src/gutter/error.dart
@@ -1,9 +1,6 @@
 import 'dart:async';
 
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 import '../analyzer/api/models/issue.dart';
 import 'clickable.dart';
