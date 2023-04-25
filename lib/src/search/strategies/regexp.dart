e22373bf15369975de2a8bf5d480b9da2aa6d827
Yestay Tastanov <yestai777@gmail.com>
2023-04-25 13:11:45 +0600

    regex strategy added

--- lib/src/search/strategies/regexp.dart
+++ lib/src/search/strategies/regexp.dart
@@ -1,11 +1,19 @@
-import '../result.dart';
-import '../settings.dart';
+import '../../../flutter_code_editor.dart';
 import 'abstract.dart';
 
 class RegExpSearchStrategy extends SearchStrategy {
   @override
   SearchResult searchPlain(String text, {required SearchSettings settings}) {
-    // TODO: implement searchPlain
-    throw UnimplementedError();
+    final regex = RegExp(settings.pattern);
+    final matches = regex.allMatches(text);
+    final searchMatches = matches
+        .map<SearchMatch>(
+          (e) => SearchMatch(
+            start: e.start,
+            end: e.end,
+          ),
+        )
+        .toList(growable: false);
+    return SearchResult(matches: searchMatches);
   }
 }
